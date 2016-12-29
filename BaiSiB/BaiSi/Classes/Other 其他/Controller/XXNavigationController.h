ac746c10bf4431fcf5bda13e2c4004c2d1a6fcc4
FuckWisdom <262377230@qq.com>
2016-12-29 21:51:02 +0800

    基本骨架调整

---
+++ BaiSiB/BaiSi/Classes/Other 其他/Controller/XXNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  XXNavigationController.h
+//  BaiSi
+//
+//  Created by harrypch on 2016/12/29.
+//  Copyright © 2016年 harrypch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XXNavigationController : UINavigationController
+
+@end
